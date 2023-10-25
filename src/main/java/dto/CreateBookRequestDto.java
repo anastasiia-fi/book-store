1bf6fc5bc1c468fe959cbc56ad4e389d25768119
Anastasiia <anastasiiafi7@gmail.com>
2023-10-25 15:18:51 +0300

    added DTO layer and mapper

---
+++ src/main/java/dto/CreateBookRequestDto.java
@@ -0,0 +1,12 @@
+package dto;
+
+import java.math.BigDecimal;
+
+public class CreateBookRequestDto {
+    private String title;
+    private String author;
+    private String isbn;
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
--- src/main/java/service/BookServiceImpl.java
+++ src/main/java/service/BookServiceImpl.java
@@ -1,24 +1,42 @@
 package service;
 
 import dao.BookRepository;
-import java.awt.print.Book;
+import dto.BookDto;
+import dto.CreateBookRequestDto;
+import exception.EntityNotFoundException;
 import java.util.List;
+import mapper.BookMapper;
+import model.Book;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookServiceImpl implements BookService {
 
     private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
-    public BookServiceImpl(BookRepository bookRepository) {
+    public BookServiceImpl(BookRepository bookRepository, BookMapper bookMapper) {
         this.bookRepository = bookRepository;
+        this.bookMapper = bookMapper;
     }
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public BookDto save(CreateBookRequestDto requestDto) {
+        Book book = bookMapper.toModel(requestDto);
+        return bookMapper.toDto(bookRepository.save(book));
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+            .map(bookMapper::toDto)
+            .toList();
+    }
+
+    @Override
+    public BookDto findById(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(() ->
+            new EntityNotFoundException("Can't find book with id " + id));
+        return bookMapper.toDto(book);
     }
 }
