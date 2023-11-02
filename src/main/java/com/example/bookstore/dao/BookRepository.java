b3c5c93151923ba59bab27f1ab5dea1a14c95613
Anastasiia <anastasiiafi7@gmail.com>
2023-11-02 10:29:50 +0200

    corrected

--- src/main/java/com/example/bookstore/dao/BookRepository.java
+++ src/main/java/com/example/bookstore/dao/BookRepository.java
@@ -2,7 +2,9 @@ package com.example.bookstore.dao;
 
 import com.example.bookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
--- src/main/java/com/example/bookstore/service/BookServiceImpl.java
+++ src/main/java/com/example/bookstore/service/BookServiceImpl.java
@@ -42,4 +42,12 @@ public class BookServiceImpl implements BookService {
     public void deleteById(Long id) {
         bookRepository.deleteById(id);
     }
+
+    @Override
+    public BookDto updateById(Long id, CreateBookRequestDto requestDto) {
+        findById(id);
+        Book book = bookMapper.toBook(requestDto);
+        book.setId(id);
+        return bookMapper.toDto(bookRepository.save(book));
+    }
 }
