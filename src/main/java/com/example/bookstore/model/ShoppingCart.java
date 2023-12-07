9931783fee09cde21a1a925aaa71b31181350b95
Anastasiia <anastasiiafi7@gmail.com>
2023-12-07 12:51:44 +0200

    corrected

--- src/main/java/com/example/bookstore/model/ShoppingCart.java
+++ src/main/java/com/example/bookstore/model/ShoppingCart.java
@@ -17,9 +17,13 @@ import java.util.Set;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Data
+@SQLDelete(sql = "UPDATE shopping_cart SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted = false")
 @Table(name = "shopping_cart")
 public class ShoppingCart {
     @Id
